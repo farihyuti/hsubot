75484c29e4d6145c831470d175613c7cc2a03615
Clara <79531054+farihyuti@users.noreply.github.com>
2021-07-12 19:27:44 +0700

    Create brainly.js

---
+++ plugins/brainly.js
@@ -0,0 +1,37 @@
+const { fetchJson } = require('../module/util');
+
+let wikipedia = {
+    name: 'wikipedia',
+    status: false,
+    clue: ['Fungsi: Wikipedia online', 'Format:\n<code>\n .wiki kata\n .wikipedia kata</code>'],
+    regex: /^[!\/\.]brain(?:ly) (.+)$/i,
+    run: function (tg, update) {
+        let message = update.message
+        let text = message.content.text.text
+
+        if (cocok = this.regex.exec(text)) {
+            tg.sendChatAction(message.chat_id)
+            let url = 'https://pencarikode.xyz/api/brainly?search='+ cocok[1] +'&apikey=Tester'
+
+            let data = {
+                method: 'get',
+                headers: { 'Content-Type': 'application/json' }
+            }
+
+            fetchJson(url, data).then(res => {
+                if (res.status == 200) {
+                    pesan = res.data.jawaban
+                } else {
+                    pesan = 'Not Found!'
+                }
+                tg.sendMessage(message.chat_id, pesan)
+            })
+
+            return true;
+        }
+    }
+}
+
+module.exports = {
+    brainly
+}
